51d628a9bda83dcdd00d8cfacf92f3a65474e548
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-05-07 13:24:06 +0800

    feat: add main functionality

---
+++ main.asm
@@ -0,0 +1,145 @@
+section .text
+    global _start
+
+_start:
+    ;call create_file
+    call print_prompt
+    call get_input
+    call print_item
+
+    call open_file_write
+    call write_file
+
+    call print_prompt
+    call get_input
+    call print_item
+
+    call write_file
+
+    call close_file
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
+
+open_file_write:
+    mov eax, 5
+    mov ebx, file_name
+    mov ecx, 0o20102
+    mov edx, 0777
+    int 0x80
+
+    mov [fd], eax
+
+    ret
+
+strlen:
+    ; input:
+    ; ecx - pointer to null-terminated string
+    ; output:
+    ; eax - length of string
+    
+    mov eax, 0  ; initialize length to 0
+    
+    .loop:
+        cmp byte [ecx], 0  ; check if current byte is null terminator
+        je .done           ; if yes, terminate loop
+        
+        inc eax            ; increment length
+        inc ecx            ; advance pointer to next byte
+        jmp .loop          ; repeat until null terminator is found
+        
+    .done:
+        ret
+
+
+close_file:
+    mov eax, 6
+    mov ebx, [fd]
+    int 0x80
+
+    ret
+
+write_file:
+    ; move file pointer to end of file
+    mov eax, 19          ; system call for lseek
+    mov ebx, [fd]       ; file descriptor
+    mov ecx, 0           ; offset from end of file (0 bytes)
+    mov edx, 2           ; seek from end of file
+    int 0x80             ; call system
+
+    mov eax, 4
+    mov ebx, [fd]
+    mov ecx, item
+    mov edx, [item_len]
+    int 0x80
+
+    ret
+
+get_input:
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, item
+    mov edx, 100
+    int 0x80
+
+    mov ecx, item
+    call strlen
+    mov [item_len], eax
+
+    ret
+
+print_item:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, item
+    mov edx, 100
+    int 0x80
+
+    ret
+
+
+create_file:
+    mov eax, 8
+    mov ebx, file_name
+    mov ecx, 0777 ; set file permission
+    int 0x80      
+    mov [fd], eax ; save file descriptor
+    
+    ret
+
+print_prompt:
+    ; print title
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, title
+    mov edx, title_len 
+    int 0x80
+
+    ; print prompt
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, prompt
+    mov edx, prompt_len
+    int 0x80
+
+    ret
+
+section .data
+    title db "To Do:", 0xa
+    title_len equ $-title
+    
+    prompt db "1 - Add", 0xa, "2 - View List", 0xa, "3 - Quit", 0xa
+    prompt_len equ $-prompt
+    
+    file_name db "test.txt", 0
+    file_name_len equ $-file_name
+
+    item times 100 db 0
+    item_len dd 0
+
+    newline db 0xa
+
+
+section .bss
+    fd resb 1
